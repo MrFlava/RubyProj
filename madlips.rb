34bde45eb9441d2095396359f7ec2172dcd7d562
MrFlava <thatelitemaili33t@gmail.com>
2025-08-12 11:28:54 +0300

    Madlips

---
+++ madlips.rb
@@ -0,0 +1,13 @@
+puts "Enter a color: "
+color = gets.chomp()
+
+puts "Enter some stuff: "
+some_stuff = gets.chomp()
+
+puts "Enter some other stuff: "
+some_other_stuff = gets.chomp()
+
+
+puts ("roses are " + color)
+puts (some_stuff + "are blue")
+puts (some_other_stuff + "nah bruh")
