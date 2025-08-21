6ee2bd216425bec595fdda90cbe491e5cd4f4bc0
MrFlava <thatelitemaili33t@gmail.com>
2025-08-21 12:59:01 +0300

    Conditions

--- ifConditions.rb
+++ ifConditions.rb
@@ -1,8 +1,12 @@
-ismale = true
+ismale = false
 istall = true
 
 if ismale and istall
     puts "You are male and tall"
+elsif ismale and !istall
+    puts "You are a short male"
+elsif !ismale and istall
+        puts "You are not male but tall"
 else
     puts "You are not male or not tall or both"
 end
