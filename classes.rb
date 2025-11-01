8c26110a16a5f5fdd848a6873aef6bbad46c1717
MrFlava <thatelitemaili33t@gmail.com>
2025-11-02 00:23:27 +0200

    Fixed

--- classes.rb
+++ classes.rb
@@ -1,21 +1,14 @@
 class Book
     attr_accessor :title, :author, :pages
-    def initialize(name)
-        puts "Creating book"
+    def initialize(title, auhtor, pages)
+        @title = title
+        @author = author
+        @pages = pages
     end
 end
 
-book1 = Book.new()
-book1.title = "Prolidze"
-book1.author = "KJ AH"
-book1.pages = 500
-
+book1 = Book.new("Prolidze", "KJ AH", 500)
 puts book1.title
 
-book2 = Book.new()
-book2.title = "Lord of South Park"
-book2.author = "AO SAP"
-book2.pages = 300
-
-
+book2 = Book.new("Lord of South Park", "AO SAP", 300)
 puts book2.title
